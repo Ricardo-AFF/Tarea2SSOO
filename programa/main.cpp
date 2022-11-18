9abc4399500c842c17c2126b483cd3ca76ca9452
Ricardo-AFF <rafloresfuentes@gmail.com>
2022-11-18 01:27:16 -0300

    Lectura txt

--- programa/main.cpp
+++ programa/main.cpp
@@ -1,5 +1,30 @@
-#include <stdio.h>
+#include <pthread.h>
+#include <iostream>
+#include <stdlib.h> // atoi
+#include <unistd.h> // sleep
+#include <fstream>
+#include <string.h>
+using namespace std;
 
-main(){
-    printf("hola mundo");
+
+int main(/*int argc, char *argv[]*/){
+//    pthread_t hebra; //almacena el identificador de la hebra
+
+    ifstream archivo;
+    string texto;
+
+    archivo.open("listadoIp.txt",ios::in);
+
+    if (archivo.fail()){
+        cout<<"No se pudo abrir el archivo";
+        exit(1);
+    }
+
+    while(!archivo.eof()){ //mientras no acabe el archivo, hacer..
+        getline(archivo,texto); // Copia lo del archivo en la variable texto
+        cout<<texto<<endl;
+    }
+
+    archivo.close();
 }
+
